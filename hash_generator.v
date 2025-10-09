a204d489569e493bba615fc79cd870d32e61e7ed
Mghzz <mohammadGhanbary1384@gmail.com>
2025-10-09 11:24:36 +0330

    Add hash_generator and ROM

--- hash_generator.v
+++ hash_generator.v
@@ -1,3 +1,18 @@
-module hash_generator();
+module hash_generator(clk , rst , start , msg , done_rnd , start_rnd, hash , done);
+    input clk , rst , start , done_rnd;
+    input [127:0] msg;
+    output start_rnd;
+    output [127:0] hash;
+
+    wire en ,  sa , sb , sc , sd , sf , loada , loadb , loadc, loadd , loadf , co;
+    wire [1:0] s1;
+    wire [5:0] out_counter;
+
+
+    hash_generator_dp d1(msg[127:96] , msg[95:64] , msg[63:32] , msg[31;0],constant,clk,rst, en ,  sa , sb , sc , sd,sf,loada, loadb , loadc, loadd,loadf,s1, hash, out_counter, co);
+
+    hash_generator_controller c1(clk , rst , start , co , done_rnd , start_rnd , en ,  sa , sb , sc , sd,sf,loada, loadb , loadc, loadd,loadf,s1 , done);
+
+
 
 endmodule 
