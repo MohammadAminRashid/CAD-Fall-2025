0598a187437ce9e52e0e75e7e2b26369ca53fb80
unknown <amin.rashid.123456@gmail.com>
2025-11-30 18:35:49 +0330

    Add ALU

--- CA3/ALU.v
+++ CA3/ALU.v
@@ -2,27 +2,109 @@
 `define Second_Quarter 2'b01            
 `define Third_Quarter  2'b10   
 `define Fourth_Quarter 2'b11                 
-                  
-module ALU #(parameter N=32)(
-input [N-1:0] A,B,C,D,
-input [1:0] opc,
 
-output reg [N-1:0] out
-);
 
- always @(A,B,C,D,opc) begin 
 
-    case(opc)
 
-    `First_Quarter: out= (B & C) | ((~B) & D) ;
-    `Second_Quarter: out= (D & B) | ((~D) & C);
-    `Third_Quarter: out= B ^ C ^ D;
-    `Fourth_Quarter : out= C ^ (B |(~D) ) ;
-    default:out=0;
+module ALU #(parameter N=8)(
+ input [N-1:0] A,B,C,D,
+ input [1:0] opc,
+ output reg [N-1:0] out
+  );
+genvar i;
+generate
+    for (i = 0; i < N; i = i + 1) begin : alu_bit
+        wire bit_out;
+        wire a_out , b_out , c_out , d_out ;
+          c1 c1_inst_a (
+                .A0(1'b1),
+                .A1(1'b0),
+                .SA(B[i]),
+                .B0(1'b1),
+                .B1(1'b0),
+                .SB(D[i]),
+                .S0(opc[0]),
+                .S1(1'b0),
+                .f(a_out)
+            );
 
-    endcase
+                c1 c1_inst_b (
+                .A0(C[i]),
+                .A1(B[i]),
+                .SA(opc[0]),
+                .B0(D[i]),
+                .B1(C[i]),
+                .SB(opc[0]),
+                .S0(a_out),
+                .S1(1'b0),
+                .f(b_out)
+            );
 
-  end
+
+                c1 c1_inst_c (
+                .A0(B[i]),
+                .A1(a_out),
+                .SA(D[i]),
+                .B0(a_out),
+                .B1(1'b1),
+                .SB(B[i]),
+                .S0(opc[0]),
+                .S1(1'b0),
+                .f(c_out)
+            );
+
+
+                c1 c1_inst_d (
+                .A0(1'b0),
+                .A1(1'b1),
+                .SA(c_out),
+                .B0(1'b1),
+                .B1(1'b0),
+                .SB(c_out),
+                .S0(C[i]),
+                .S1(1'b0),
+                .f(d_out)
+            );
+
+                c1 c1_inst_e (
+                .A0(b_out),
+                .A1(b_out),
+                .SA(1'b0),
+                .B0(d_out),
+                .B1(d_out),
+                .SB(1'b0),
+                .S0(opc[1]),
+                .S1(1'b0),
+                .f(bit_out)
+            );
+        assign out[i] = bit_out;
+    end
+endgenerate
+
 endmodule
+
+
+
+// module ALU_p #(parameter N=8)(
+// input [N-1:0] A,B,C,D,
+// input [1:0] opc,
+
+// output reg [N-1:0] out
+// );
+
+//  always @(A,B,C,D,opc) begin 
+
+//     case(opc)
+
+//     `First_Quarter: out= (B & C) | ((~B) & D) ;
+//     `Second_Quarter: out= (D & B) | ((~D) & C);
+//     `Third_Quarter: out= B ^ C ^ D;
+//     `Fourth_Quarter : out= C ^ (B |(~D) ) ;
+//     default:out=0;
+
+//     endcase
+
+//   end
+// endmodule
 
 
---
+++ CA3/ALU_TB.v
@@ -0,0 +1,59 @@
+`timescale 1ns/1ps
+
+module tb_ALU;
+
+    parameter N = 8;
+
+    reg [N-1:0] A, B, C, D;
+    reg [1:0] opc;
+
+    wire [N-1:0] out_gen;
+    wire [N-1:0] out_p;
+
+    integer t, i;
+
+ 
+    ALU #(N) alu_gen_inst(
+        .A(A),
+        .B(B),
+        .C(C),
+        .D(D),
+        .opc(opc),
+        .out(out_gen)
+    );
+
+    ALU_p #(N) alu_p_inst(
+        .A(A),
+        .B(B),
+        .C(C),
+        .D(D),
+        .opc(opc),
+        .out(out_p)
+    );
+
+    initial begin
+        for (t = 0; t < 100; t = t + 1) begin
+
+            A = $random;
+            B = $random;
+            C = $random;
+            D = $random;
+
+            for (i = 0; i < 4; i = i + 1) begin
+                opc = i;
+                #5;
+
+                if (out_gen !== out_p) begin
+                    $display("❌ MISMATCH at test=%0d opc=%b\n A=%h B=%h C=%h D=%h \n out_gen=%h out_p=%h",
+                              t, opc, A, B, C, D, out_gen, out_p);
+                end else begin
+                    $display("✔ MATCH test=%0d opc=%b out=%h", t, opc, out_gen);
+                end
+            end
+        end
+
+        $display("===== All tests finished =====");
+        $stop;
+    end
+
+endmodule
