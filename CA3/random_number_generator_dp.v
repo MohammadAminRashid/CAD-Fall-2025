f4159a306edaa2c4c1d9f042472089d74dbabeaa
unknown <amin.rashid.123456@gmail.com>
2025-12-01 11:44:11 +0330

    Add xor to rng dp

--- CA3/random_number_generator_dp.v
+++ CA3/random_number_generator_dp.v
@@ -6,12 +6,15 @@ output co
 );
 
 wire SerIn;
+wire w1;
 wire[5:0] out;
 wire [2:0] out_counter;
 shift_register #(6) sr(clk , en_SR ,load_SR , SerIn , par_load , out );
 counter #(3) count(clk,rst_count,en_count,3'b101 , co , out_counter);
 
-xor(SerIn , out[1],out[3] ,out[5]);
+// xor(SerIn , out[1],out[3] ,out[5]);
+c1 c1_inst_1(1'b0 , 1'b1 , out[1] , 1'b1 , 1'b0 , out[1] , out[3] , 1'b0 , w1);
+c1 c1_inst_2(1'b0 , 1'b1 , w1 , 1'b1 , 1'b0 , w1 , out[5] , 1'b0 , SerIn);
 assign rnd=out[5:4];
 
 endmodule
