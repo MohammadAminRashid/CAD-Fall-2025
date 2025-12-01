5628fa5dd92438e3e2b7927a8792f01c514835ff
unknown <amin.rashid.123456@gmail.com>
2025-12-01 11:34:51 +0330

    Add mux 4to1

--- CA3/mux4to1.v
+++ CA3/mux4to1.v
@@ -3,10 +3,22 @@ module mux4to1 #(parameter N = 32) (
     input [1:0]select,            
     output [N-1:0] Y     
 );
-    assign Y = (select==2'b00) ? A :
-               (select==2'b01) ? B :
-               (select==2'b10) ? C :
-               (select==2'b11) ? D :
-               {N{1'b0}};
+
+genvar i;
+generate
+    for (i = 0; i < N; i = i + 1) begin : mux_block
+        c1 mux_inst (
+            .A0(A[i]),
+            .A1(B[i]),
+            .SA(select[0]),
+            .B0(C[i]),
+            .B1(D[i]),
+            .SB(select[0]),
+            .S0(select[1]),
+            .S1(1'b0),
+            .f(Y[i])
+        );
+    end
+endgenerate
                
 endmodule
