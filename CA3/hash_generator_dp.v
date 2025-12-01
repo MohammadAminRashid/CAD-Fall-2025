9d6b259184317e4ef73f4ab03f85f9c232332a4d
Mghzz <mohammadGhanbary1384@gmail.com>
2025-12-01 16:07:10 +0330

    Fix cin bug

--- CA3/hash_generator_dp.v
+++ CA3/hash_generator_dp.v
@@ -11,9 +11,8 @@ module hash_generator_dp #(parameter WORD_WIDTH = 8)(
 
     wire [4*WORD_WIDTH-1:0] M;
     wire [WORD_WIDTH-1:0] A, B, C, D, F, f0;
-    wire [WORD_WIDTH-1:0] adder_out_1, adder_out_2, adder_out_3, adder_out_4;
+    wire [WORD_WIDTH-1:0] adder_out_1, adder_out_2;
     wire [WORD_WIDTH-1:0] mux4to1_out, mult_out;
-    wire [WORD_WIDTH-1:0] parload_a, parload_b, parload_c, parload_d, par_load_f;
 
     counter #(6) count (clk, rst ,rst, en,6'b000000,co,out_counter);
 
@@ -25,7 +24,7 @@ module hash_generator_dp #(parameter WORD_WIDTH = 8)(
 
     register #(4*WORD_WIDTH) Mreg(clk, loadm, rst, message, M);
 
-    adder #(WORD_WIDTH) adder1(mult_out, B, adder_out_1);
+    adder #(WORD_WIDTH) adder1(mult_out, B, 1'b0 ,  adder_out_1);
     adder4 #(WORD_WIDTH) adder2 (A , constant , F , mux4to1_out , 1'b0 , adder_out_2);
 
     mux4to1 #(WORD_WIDTH) mux_msg (M[4*WORD_WIDTH-1:3*WORD_WIDTH],M[3*WORD_WIDTH-1:2*WORD_WIDTH],M[2*WORD_WIDTH-1:WORD_WIDTH],M[WORD_WIDTH-1: 0],s1,mux4to1_out);
